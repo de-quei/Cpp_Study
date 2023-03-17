9e41e95105466cf103fb8a6627b6d2ae12a1e18b
Hyunji Kim <papertoy01@gmail.com>
2023-03-17 09:44:51 +0900

    namespace

--- 2023_Cpp_202/grammer/C++기본.cpp
+++ 2023_Cpp_202/grammer/C++기본.cpp
@@ -1,11 +1,14 @@
 ﻿// cout객체
 #include <iostream>
 
+//변수 이름의 중복 방지
+using namespace std;
+
 int main() {
 
 	// << 출력 연산자
 	// endl : 개행
-	std::cout << "hello world\n" << std::endl;
+	cout << "hello world" << endl;
 
 	return 0;
 
