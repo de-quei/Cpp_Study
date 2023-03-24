89213c7056a45a2e603b4e81d99799be47857d83
Hyunji Kim <papertoy01@gmail.com>
2023-03-24 09:51:01 +0900

    strlen (C++ style) & size()

--- 2023_Cpp_202/grammer/문자열 길이.cpp
+++ 2023_Cpp_202/grammer/문자열 길이.cpp
@@ -9,10 +9,11 @@
 using namespace std;
 
 int main(void) {
-	char str1[30] = "Jiwoo";
-	int len = strlen(str1);
 
-	printf("문자열의 길이 : %d\n", len);
+	string str1 = "Jiwoo";
+
+	// str1.length()도 가능하지만, size()를 권장
+	cout << "문자열의 길이 : " << str1.size() << endl;
 
 	return 0;
 }
