390cd3185645f3d494bd34f833e8c5f983c77bd5
Hyunji Kim <papertoy01@gmail.com>
2023-03-28 14:23:54 +0900

    .

---
+++ 2023_Cpp_202/grammer/문자열 비교.cpp
@@ -0,0 +1,47 @@
+﻿// C
+#include <stdio.h>
+#include <string.h>
+
+//C++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+
+	char str1[50] = "Interface";
+	char str2[50] = "Inheritance";
+
+	const int s1_l = strlen(str1);
+	const int s2_l = strlen(str2);
+
+	const int l = s1_l < s2_l ? s1_l : s2_l;
+
+	int i = 0;
+	while (i < 1) {
+		if (str1[i] > str2[i]) {
+			printf("str1 > str2");
+			break;
+		}
+		else if (str1[i] < str2[i]) {
+			printf("str1 < str2");
+			break;
+		}
+		i++;
+	}
+
+	if (i == 1) {
+		if (s1_l > s2_l) {
+			printf("str1 > str2");
+		}
+		else if (s1_l < s2_l) {
+			printf("str1 < str2");
+		}
+		else {
+			printf("str1 == str2");
+		}
+	}
+	
+	return 0;
+}
