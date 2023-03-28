fed5f604acf8925cd287ed67bdc10831a8c037c5
Hyunji Kim <papertoy01@gmail.com>
2023-03-28 14:42:40 +0900

    Cpp style 문자열비교

--- 2023_Cpp_202/grammer/문자열 비교.cpp
+++ 2023_Cpp_202/grammer/문자열 비교.cpp
@@ -2,46 +2,28 @@
 #include <stdio.h>
 #include <string.h>
 
-//C++
+// C++
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int main(void) {
+int main(void)
+{
+	string str1 = "Hello";
+	string str2 = "World";
 
-	char str1[50] = "Interface";
-	char str2[50] = "Inheritance";
-
-	const int s1_l = strlen(str1);
-	const int s2_l = strlen(str2);
-
-	const int l = s1_l < s2_l ? s1_l : s2_l;
-
-	int i = 0;
-	while (i < 1) {
-		if (str1[i] > str2[i]) {
-			printf("str1 > str2");
-			break;
-		}
-		else if (str1[i] < str2[i]) {
-			printf("str1 < str2");
-			break;
-		}
-		i++;
+	if (str1 == str2) {
+		cout << "str1 == str2";
+	}
+	// str2가 str1보다 사전에 늦게나오는 경우
+	else if (str1 < str2) {
+		cout << "str1 < str2";
+	}
+	else {
+		cout << "str1 > str2";
 	}
 
-	if (i == 1) {
-		if (s1_l > s2_l) {
-			printf("str1 > str2");
-		}
-		else if (s1_l < s2_l) {
-			printf("str1 < str2");
-		}
-		else {
-			printf("str1 == str2");
-		}
-	}
-	
+
 	return 0;
 }
