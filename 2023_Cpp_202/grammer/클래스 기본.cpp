5a9b7a761292c454153f4c50176a4d35a316d9f8
Hyunji Kim <papertoy01@gmail.com>
2023-03-31 08:57:07 +0900

    구조체 기본

---
+++ 2023_Cpp_202/grammer/클래스 기본.cpp
@@ -0,0 +1,26 @@
+﻿// C
+#include <stdio.h>
+#include <string.h>
+
+// C++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct pizza {
+	string brand;
+	string menu;
+};
+int main(void)
+{
+
+	struct pizza todaysPizza;
+	todaysPizza.brand = "Pizza school";
+	todaysPizza.menu = "Combination";
+
+	cout << "Brand : " << todaysPizza.brand << endl;
+	cout << "Brand : " << todaysPizza.menu << endl;
+
+	return 0;
+}
