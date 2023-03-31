5d87ab81f1830049aa4bb171775dd7976c42ebe8
Hyunji Kim <papertoy01@gmail.com>
2023-03-31 09:53:12 +0900

    class, 접근제어자 public

--- 2023_Cpp_202/grammer/클래스 기본.cpp
+++ 2023_Cpp_202/grammer/클래스 기본.cpp
@@ -10,10 +10,12 @@ using namespace std;
 
 // 여러가지 data들을 하나의 의미있는 구조체로 묶어서 관리
 // 구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 합시다
-struct Student {
+// class는 default가 private (struct는 default가 public)
+class Student {
 	// 성능때문에 학번은 문자열로 하지 않음.
-	//일반적으로 문자열은 정수형 데이터보다 많은 메모리 공간을 요귷며
-	//정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자 수 만큼 반복하여 비교해야함.
+	// 일반적으로 문자열은 정수형 데이터보다 많은 메모리 공간을 요귷며
+	// 정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자 수 만큼 반복하여 비교해야함.
+public:
 	int number;			//학번
 	string name;		//이름
 	string tel;			//전화번호
@@ -29,7 +31,7 @@ int main(void)
 	student.department = "뉴미디어소프트웨어과";
 	student.address = "제주특별자치도 제주시 노형동 월산북길 77, 노형휴팰리스 2302호";
 
-	//자료형 : struct Student (C++에서는 Student만 써도 가능)
+	//자료형 : Student (class 생략가능)
 	cout << "학    번 : " << student.number << endl;
 	cout << "이    름 : " << student.name << endl;
 	cout << "전화번호 : " << student.tel << endl;
