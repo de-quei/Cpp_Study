ebb3c8d5c30067c515272875ce1e805702ab8ac9
Hyunji Kim <papertoy01@gmail.com>
2023-04-18 13:32:18 +0900

    setter

--- 2023_Cpp_202/grammer/클래스 기본.cpp
+++ 2023_Cpp_202/grammer/클래스 기본.cpp
@@ -1,98 +1,90 @@
-﻿// C
-#include <stdio.h>
-#include <string.h>
-
-// C++
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 
 using namespace std;
 
-struct Person {
-	int height;
-	int weight;
-};
-// 여러가지 data들을 하나의 의미있는 구조체로 묶어서 관리
-// 구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 합시다
-// class는 default가 private (struct는 default가 public)
+// 여러 data들을 하나의 의미있는 구조체로 묶어서 관리
+// 구조체, 클래스는 일반적으로 단어의 첫 글자로 대문자로 합시다
+// class는 디폴트가 private (stucrt는 디폴트가 public)
 class Student {
-
 public:
-	// 생성자 (Constructor) : 객체를 생성할 때 호출되는 함수
-	// 생성자는 반환형을 쓰지 않는다.
-	// 생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다.
-	Student() {
-		number = 2208;
-		name = "김현지";
-		tel = "010-2952-3435";
-		department = "뉴미디어소프트웨어과";
-		address = "제주특별자치도 제주시 노형동 월산북길 77, 노형휴팰리스 2302호";
-	}
+	// 생성자(constructor) : 객체가 생성될 때 호출되는 함수
+	// 생성자는 반환형을 쓰지 않는다
+	// 생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
+	Student() {}
 
-	Student(int _number, string _name, string _tel, string _department, string _address) 
-		// 멤버변수 초기화. const / 참조형 멤버변수 사용 가능
-		: number(_number), name(_name), tel(_tel), department(_department), address(_address)
+	Student(int _hakbun, string _name, string _tel, string _department, string _address)
+		// 멤버변수 초기화. const/참조형 멤버변수 사용가능
+		: hakbun(_hakbun), name(_name), tel(_tel)
+		, department(_department), address(_address)
 	{
 	}
 
-	void print(void) {
-		//자료형 : Student (class 생략가능)
-		cout << "학    번 : " <<  number << endl;
-		cout << "이    름 : " <<  name << endl;
-		cout << "전화번호 : " <<  tel << endl;
-		cout << "학    과 : " <<  department << endl;
-		cout << "주    소 : " <<  address << endl;
+	// class 멤버 함수를 가질 수 있다
+	void print(void)
+	{
+		cout << "학번 : " << hakbun << endl;
+		cout << "이름 : " << name << endl;
+		cout << "전화번호 : " << tel << endl;
+		cout << "학과 : " << department << endl;
+		cout << "주소 : " << address << endl;
 	}
+
+	void set_hakbun(int _hakbun) { hakbun = _hakbun; }
+	void set_name(string _name) { name = _name; }
+	void set_tel(string _tel) { tel = _tel; }
+	void set_department(string _department) { department = _department; }
+	void set_address(string _address) { address = _address; }
 
 private:
-	// 성능때문에 학번은 문자열로 하지 않음.
+	// 성능때문에 hakbun을 문자열로 하지 않음
 	// 일반적으로 문자열은 정수형 데이터보다 많은 메모리 공간을 요구하며
-	// 정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자 수 만큼 반복하여 비교해야함.
-	int number;			//학번
-	string name;		//이름
-	string tel;			//전화번호
-	string department;	//학과
-	string address;		//주소
-
+	// 정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
+	int hakbun;
+	string name;
+	string tel;
+	string department;
+	string address;
 };
+
 int main(void)
 {
-	//매개변수가 없는 생성자
-	//Student student;
-	//Student student1 = Student();
-	//student.print();
+	// 매개변수가 없는 생성자
+	// Student stu1;
+	// Student stu1 = Student();
+	// stu1.print();
 
-	// 정적할당 : 컴파일 시간에 메모리 크기 결정
-	//Student student2 = Student(1234, "오모리모토키", "010-1212-3434", "MRS.GREENAPPLE", "일본");
-	//student2.print();
+	// 정적할당 : 컴파일 시간에 메모리 크기가 결정
+	// Student stu2 = Student(2200, "류이치", "010-0000-0000", "뉴미디어소프트웨어", "일본어딘가");
+	// stu2.print();
 
-	Student student3[2];
+	//Student stu3[2];
+	//for (int i = 0; i < 2; i++)
+	//	stu3[i].print();
 
-	for (int i = 0; i < sizeof(student3)/sizeof(Student); i++) {
-		student3[i].print();
-	}
+	// 동적할당 : 실행시간(runtime)에 메모리 크기가 결정(메모리 heap영역)
+	// 동적할당된 공간은 포인터로 접근한다
+	//Student* stu3 = new Student(22, "율곡", "010-0000-0000", "유교", "한성");
+	//stu3->print();		// stu3의 멤버는 ->로 접근
+	//// 동적할당 해제(안하면 메모리 누수현상이 발생)
+	//delete stu3;
 
-	Student* student4 = new Student[2];
-	for (int i = 0; i < sizeof(student4) / sizeof(Student); i++) {
-		
-	}
-	//struct Person p;
-	//p.height = 163;
-	//p.weight = 50;
+	Student* stu4 = new Student[2];
+	stu4[0].set_name("박진영");
+	stu4[0].set_address("니가사는그집");
+	stu4[0].set_hakbun(1111);
+	stu4[0].set_department("뉴미디어소프트웨어");
+	stu4[0].set_tel("000-0000-0000");
 
-	//struct Person* ptr = &p;
+	stu4[1].set_name("선미");
+	stu4[1].set_address("서울어딘가");
+	stu4[1].set_hakbun(1112);
+	stu4[1].set_department("뉴미디어소프트웨어");
+	stu4[1].set_tel("000-0000-0001");
 
-	// 둘 다 같은 결과
-	//cout << "키       : " << ptr->height << "cm" << endl;
-	//cout << "몸 무 게 : " << (*ptr).weight << "kg" << endl;
-
-	//동적할당은 포인터로 접근한다.
-	//실행 시간에 메모리 크기 결정 / heap영역에 할당
-	//Student* student3 = new Student(1234, "오모리모토키", "010-1212-3434", "MRS.GREENAPPLE", "도쿄");
-	//student3->print();
-
-	///동적할당 해제 (안하면 메모리 누수현상 발생)
-	//delete student3;
+	for (int i = 0; i < 2; i++)
+		stu4[i].print();	// 배열의 요소에 해당하는 객체는 멤버를 .으로 접근
+	delete[] stu4;
 
 	return 0;
 }
