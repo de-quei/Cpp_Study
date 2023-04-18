d057bc1bd58472e3a38ab3e99441821890de4aff
Hyunji Kim <papertoy01@gmail.com>
2023-04-18 14:41:50 +0900

    멤버변수 동적할당

--- 2023_Cpp_202/grammer/클래스 중급.cpp
+++ 2023_Cpp_202/grammer/클래스 중급.cpp
@@ -1,18 +1,22 @@
 ﻿#include <iostream>
-#include <string>
+#include <string.h>
 using namespace std;
 
 class MString {	//class는 기본이 private
 public:
-	unsigned int size(void) { return size_; }
-	const char* c_str(void) { return c_str_; }
 	MString(const char* str) {	//생성자
-		c_str_ = str;
-		size_ = strlen(str);//문자열 길이 strlen함수
+		unsigned int l = strlen(str); //문자열의 길이
+		//동적할당!
+		c_str_ = new char[l + 1];	// '\0' 널문자가 들어갈 공간 +1
+		strcpy(c_str_, str);		// 문자열 복사
 	}
+	unsigned int size(void) { return size_; }
+	char* c_str(void) { return c_str_; }
+
 private:
 	unsigned int size_;	//문자열의 길이
-	const char* c_str_;	//문자열의 시작 주소
+	char* c_str_;	//문자열의 시작 주소
+	//cpp은 const를 선언해주면 그 변수를 사용할 때 마다 const를 붙여줘야 한다.
 };
 int main(){
 
