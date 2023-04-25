72ad34fc1dbc8b07201412567228e2c604a6cea7
Hyunji Kim <papertoy01@gmail.com>
2023-04-25 13:37:17 +0900

    소멸자로 생성자에서 동적할당한 공간해제

--- 2023_Cpp_202/grammer/클래스 중급.cpp
+++ 2023_Cpp_202/grammer/클래스 중급.cpp
@@ -14,6 +14,12 @@ public:
 	unsigned int size(void) { return size_; }
 	char* c_str(void) { return c_str_; }
 
+	//소멸자(destructor)
+	~MString() {
+		//소멸자로 생성자에서 동적할당한 메모리 해제
+		delete[] c_str_;
+	}
+
 private:
 	unsigned int size_;	//문자열의 길이
 	char* c_str_;	//문자열의 시작 주소
