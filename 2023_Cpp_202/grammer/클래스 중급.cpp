6a7d7cb62f3d0a0f907f3c446e99713ef1fee61e
Hyunji Kim <papertoy01@gmail.com>
2023-04-25 13:44:20 +0900

    생성자와 소멸자 콘솔로 확인

--- 2023_Cpp_202/grammer/클래스 중급.cpp
+++ 2023_Cpp_202/grammer/클래스 중급.cpp
@@ -10,6 +10,7 @@ public:
 		c_str_ = new char[l + 1];	// '\0' 널문자가 들어갈 공간 +1
 		strcpy(c_str_, str);		// 문자열 복사
 		size_ = l;
+		cout << "MString 생성자 호출 완료" << endl;
 	}
 	unsigned int size(void) { return size_; }
 	char* c_str(void) { return c_str_; }
@@ -18,6 +19,7 @@ public:
 	~MString() {
 		//소멸자로 생성자에서 동적할당한 메모리 해제
 		delete[] c_str_;
+		cout << "MString 소멸자 호출 완료" << endl;
 	}
 
 private:
