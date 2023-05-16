320e85ec686990a33c4f4115bbe3aa7c91abd200
Hyunji Kim <papertoy01@gmail.com>
2023-05-16 14:36:27 +0900

    깊은복사

--- 2023_Cpp_202/grammer/복사생성자.cpp
+++ 2023_Cpp_202/grammer/복사생성자.cpp
@@ -12,6 +12,14 @@ public:
 		strcpy(str_, jhqz);
 		cout << "일반생성자 호출" << endl;
 	}
+	
+	//복사생성자(얕은복사)
+	Munja(const Munja& hj)
+	{
+		str_ = new char[strlen(hj.str_) + 1];
+		strcpy(str_, hj.str_);
+		cout << "일반생성자 호출" << endl;
+	}
 
 	~Munja()
 	{
@@ -24,5 +32,7 @@ private:
 int main()
 {
 	Munja m1 = Munja("abc");	//일반생성자 호출
+	Munja m2 = m1;				//복사생성자 호출
+
 	return 0;
 }
