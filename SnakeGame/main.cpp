7facaf579b2b91f3fe4922d4ed2f9d98157049f4
Hyunji Kim <papertoy01@gmail.com>
2023-05-19 10:05:03 +0900

    [SFML] 창 띄우기

---
+++ SnakeGame/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main(void) 
+{
+	RenderWindow window(VideoMode(640, 480), "Snake Game");
+	while (window.isOpen()) 
+	{
+		Event e;
+		while (window.pollEvent(e)) 
+		{
+			//window의 close를 눌렀을 때 창을 닫는다.
+			if (e.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
+	return 0;
+}
