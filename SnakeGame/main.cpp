7f674d855bbdb4d5496d8c7cadac6cd3b9001c61
Hyunji Kim <papertoy01@gmail.com>
2023-05-23 13:58:13 +0900

    사과 만들기

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -14,6 +14,11 @@ int main(void)
 	snake.setPosition(100, 300);
 	snake.setSize(Vector2f(50, 50));
 
+	RectangleShape apple;
+	apple.setFillColor(Color::Red);
+	apple.setPosition(200, 400);
+	apple.setSize(Vector2f(50, 50));
+
 	while (window.isOpen()) 
 	{
 		Event e;
@@ -35,6 +40,7 @@ int main(void)
 
 		window.clear();
 
+		window.draw(apple);
 		window.draw(snake);
 
 		window.display();
