34c22a4e6df91073ab20b3feb035a9cf7b22ea8a
Hyunji Kim <papertoy01@gmail.com>
2023-06-09 09:50:00 +0900

    부모의 멤버변수/함수 사용

--- 2023_Cpp_202/grammer/상속.cpp
+++ 2023_Cpp_202/grammer/상속.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 class Animal {
 public:
-	Animal() { cout << "동물 생성자" << endl; }
+	Animal(int age, string name) : age_(age), name_(name) {
+		cout << "동물 생성자" << endl; 
+		cout << "나이 : " << age_ << endl;
+		cout << "이름 : "<< name_ << endl;
+	}
 	~Animal() { cout << "동물 소멸자" << endl; }
 	void Bark(void) { cout << "동물이 짖는다." << endl; }
 	void Eat(void) { cout << "동물이 먹는다." << endl; }
@@ -13,22 +17,26 @@ public:
 
 private:
 	int age_;
-	int name_;
+	string name_;
 };
 
 //두루미
 class Crane : public Animal{
 public:
-	//별도 언급이 없어도 매개변수가 없는 부모생성자가 호출된다.
-	Crane() { cout << "두루미 생성자" << endl; }
+	Crane(int age, string name, int leg_length) : Animal(age, name) { 
+		cout << "두루미 생성자" << endl; 
+		leg_length_ = leg_length;
+		cout << "다리 길이 : " << leg_length_ << endl;
+	}
 	~Crane() { cout << "두루미 소멸자" << endl; }
 
 private:
-	int leg_length;
+	int leg_length_;
 };
 int main() {
 
-	Crane* crane = new Crane();
+	Crane* crane = new Crane(13, "두두두두두", 30);
+	crane->Eat(); //부모의 멤버함수 사용
 
 	delete crane;
 
