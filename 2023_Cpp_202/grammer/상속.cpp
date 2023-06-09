076b5072c26cd5a7c50b701997f3ce7f091dba6b
Hyunji Kim <papertoy01@gmail.com>
2023-06-09 09:17:09 +0900

    상속에 따른 생성/소멸

---
+++ 2023_Cpp_202/grammer/상속.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Animal {
+public:
+	Animal() { cout << "동물 생성자" << endl; }
+	~Animal() { cout << "동물 소멸자" << endl; }
+	void Bark(void) { cout << "동물이 짖는다." << endl; }
+	void Eat(void) { cout << "동물이 먹는다." << endl; }
+	void Hunt(void) { cout << "동물이 사냥한다." << endl; }
+
+private:
+	int age_;
+	int name_;
+};
+
+//두루미
+class Crane : public Animal{
+public:
+	//별도 언급이 없어도 매개변수가 없는 부모생성자가 호출된다.
+	Crane() { cout << "두루미 생성자" << endl; }
+	~Crane() { cout << "두루미 소멸자" << endl; }
+
+private:
+	int leg_length;
+};
+int main() {
+
+	Crane* crane = new Crane();
+
+	delete crane;
+
+	return 0;
+}
